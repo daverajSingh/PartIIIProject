88e2e13725eae05ad9a37a01eb9e80c7be7e60a9
daveraj <daveraj.singh@outlook.com>
2024-03-28 00:08:13 +0000

    created program for preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def preprocess_data(data):
+    """
+    Preprocesses the data by converting images to grayscale along the red channel, then reducing size of images to 1/6 of original size.
+    
+    Args:
+    data: numpy array of shape
+    
+    Returns:
+    numpy array of shape 
+    """ 
+    
+    processed_data = []
+    
+    return processed_data
